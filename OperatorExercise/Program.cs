113a62eb7636e97927db980049b8ed19f9ae4728
KillSwitch999 <140569515+KillSwitch999@users.noreply.github.com>
2025-05-27 17:33:54 -0400

    Initial commit

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+
+            var sum = a + b;
+            Console.WriteLine(sum);
+
+        int quotient = a / b;
+            int remainder = a % b;
+            Console.WriteLine(quotient);
+            Console.WriteLine(remainder);
+            
+            //Circle radius exercise-User input required
+            Console.WriteLine("Type in a value for a radius of a circle below: ");
+            double radius = double.Parse(Console.ReadLine());
+            double area = AreaOfCircle(radius);
+            Console.WriteLine($"The area of the circle with a radius of {radius} is {area}");
+        }
+
+        static double AreaOfCircle(double radius)
+        {
+            return Math.PI * Math.Pow(radius, 2);
         }
     }
 }
